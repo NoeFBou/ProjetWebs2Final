7d2b650834c3456c7d8a447150fd66a3bc4e2159
NoeFBou <102726520+NoeFBou@users.noreply.github.com>
2025-02-10 11:41:53 +0100

    slider

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
@@ -13,6 +15,44 @@ app.listen(PORT, () => {
 });
 
 let assignments = require('./data.json');
+
+//route pour récupérer tous les assignments
+app.get('/api/assignments', (req, res) => {
+    res.send(assignments);
+});
+
+//route pour récupérer un seul assignment
+app.get('/api/assignments/:id', (req, res) => {
+    let id = req.params.id;
+    let assignment = assignments.find(assignment => assignment.id === id);
+    res.send(assignment);
+});
+
+//route pour ajouter un assignment
+app.post('/api/assignments', (req, res) => {
+    let assignment = req.body;
+    assignment.id = assignments.length + 1;
+    assignments.push(assignment);
+    res.send(assignment);
+});
+
+//route pour modifier un assignment
+app.put('/api/assignments/:id', (req, res) => {
+    let id = req.params.id;
+    let assignment = assignments.find(assignment => assignment.id === id);
+    assignment.nom = req.body.nom;
+    assignment.dateDeRendu = req.body.dateDeRendu;
+    res.send(assignment);
+});
+
+//route pour supprimer un assignment
+app.delete('/api/assignments/:id', (req, res) => {
+    let id = req.params.id;
+    assignments = assignments.filter(assignment => assignment.id !== id);
+    res.send(assignments);
+});
+
+
 
 const multer = require('multer');
 const upload = multer();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Box, VStack, HStack, Text } from "@chakra-ui/react";
+import {Box, HStack, Text} from "@chakra-ui/react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import dynamic from "next/dynamic";
+import {useEffect, useState} from "react";
+//import Slider from "react-slick";
+
 
 const Slider = dynamic(() => import("react-slick").then((m) => m.default), {
     ssr: false
 });
 
 type SlideProps = {
-    title: string;
+    stock_name?: string,
+    stock_industry?: string,
+    stock_sector?: string,
+    stock_market_cap?: string,
+    department?: string,
+    address?: string,
+    id?: number
 };
 
-const Slide = ({ title }: SlideProps) => {
+
+const Slide = ({stock_name, stock_industry, stock_sector, stock_market_cap, department, address, key}: SlideProps) => {
     return (
         <HStack
             w="１００％"
@@ -27,13 +34,47 @@ const Slide = ({ title }: SlideProps) => {
             mx="4"
         >
             <Text color="white" fontWeight="bold">
-                {title}
+                {stock_name}
             </Text>
+            {stock_industry && (
+                <Text color="white" fontWeight="bold">
+                    {stock_industry}
+                </Text>
+            )}
+            {stock_sector && (
+                <Text color="white" fontWeight="bold">
+                    {stock_sector}
+                </Text>
+            )}
+            {stock_market_cap && (
+                <Text color="white" fontWeight="bold">
+                    {stock_market_cap}
+                </Text>
+            )}
+            {department && (
+                <Text color="white" fontWeight="bold">
+                    {department}
+                </Text>
+            )}
+            {address && (
+                <Text color="white" fontWeight="bold">
+                    {address}
+                </Text>
+            )}
         </HStack>
     );
 };
 
 function App() {
+
+    const [assignments, setAssignments] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/assignments')
+            .then(response => response.json())
+            .then(data => setAssignments(data))
+            .catch(error => console.error("Erreur lors de la récupération des données : ", error));
+    }, []);
 
     const slickSettings = {
         dots: true,
@@ -43,67 +84,75 @@ function App() {
         slidesToShow: 3
     };
 
-  return (
-      <>
-          <div className="App">
-              <Box m="20">
-                  <Box
-                      sx={{
-                          ".slick-dots": {
-                              transform: "translateY(1em)"
-                          },
-                          ".slick-dots li button": {
-                              _before: {
-                                  transition: "0.2s",
-                                  content: "''",
-                                  borderRadius: "100%",
-                                  background: "cyan.500"
-                              }
-                          },
-                          ".slick-arrow": {
-                              backgroundColor: "#950d36",
-                              color: "white",
-                              w: "30px",
-                              h: "50px",
-                              transition: "0.2s",
-                              _hover: {
-                                  backgroundColor: "#950d36",
-                                  color: "white"
-                              },
-                              _focus: {
-                                  backgroundColor: "#ef430e",
-                                  color: "white"
-                              },
-                              _before: {
-                                  transition: "0.2s"
-                              }
-                          },
-                          ".slick-prev": {
-                              left: "-40px",
-                              _before: {
-                                  content: '"◀"'
-                              }
-                          },
-                          ".slick-next": {
-                              right: "-40px",
-                              _before: {
-                                  content: '"▶"'
-                              }
-                          }
-                      }}
-                  >
-                      <Slider {...slickSettings}>
-                          <Slide title={"スライド１"}/>
-                          <Slide title={"スライド２"}/>
-                          <Slide title={"スライド３"}/>
-                          <Slide title={"スライド４"}/>
-                          <Slide title={"スライド５"}/>
-                      </Slider>
-                  </Box>
-              </Box>
-          </div>
-      </>
-  )
+    return (
+        <div className="App">
+            <Box m="20">
+                <Box
+                    sx={{
+                        ".slick-dots": {
+                            transform: "translateY(1em)"
+                        },
+                        ".slick-dots li button": {
+                            _before: {
+                                transition: "0.2s",
+                                content: "''",
+                                borderRadius: "100%",
+                                background: "cyan.500"
+                            }
+                        },
+                        ".slick-arrow": {
+                            backgroundColor: "#950d36",
+                            color: "white",
+                            w: "30px",
+                            h: "50px",
+                            transition: "0.2s",
+                            _hover: {
+                                backgroundColor: "#950d36",
+                                color: "white"
+                            },
+                            _focus: {
+                                backgroundColor: "#ef430e",
+                                color: "white"
+                            },
+                            _before: {
+                                transition: "0.2s"
+                            }
+                        },
+                        ".slick-prev": {
+                            left: "-40px",
+                            _before: {
+                                content: '"◀"'
+                            }
+                        },
+                        ".slick-next": {
+                            right: "-40px",
+                            _before: {
+                                content: '"▶"'
+                            }
+                        }
+                    }}
+                >
+                    <Slider {...slickSettings}>
+                        {assignments.length > 0 ? (
+                            assignments.map((assignment : SlideProps) => (
+                                <Slide
+                                    key={assignment.id}
+                                    stock_name={assignment.stock_name}
+                                    stock_industry={assignment.stock_industry}
+                                    stock_sector={assignment.stock_sector}
+                                    stock_market_cap={assignment.stock_market_cap}
+                                    department={assignment.department}
+                                    address={assignment.address}
+                                />
+                            ))
+                        ) : (
+                            <Slide stock_name="Chargement..."/>
+                        )}
+                    </Slider>
+                </Box>
+            </Box>
+        </div>
+    )
 }
 
 export default App
