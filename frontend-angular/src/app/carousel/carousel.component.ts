be43b58dcb4683cbef09e11990ccd0b63df5f745
NoeFBou <102726520+NoeFBou@users.noreply.github.com>
2025-03-03 15:39:59 +0100

    aled

--- frontend-angular/src/app/carousel/carousel.component.ts
+++ frontend-angular/src/app/carousel/carousel.component.ts
@@ -1,20 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {Assignment, AssignmentService} from "../assignment.service";
-import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import {NgbCarouselModule} from "@ng-bootstrap/ng-bootstrap";
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [
-    FormsModule
-  ],
+  imports: [CommonModule, NgbCarouselModule],
+
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
   assignments: Assignment[] = [];
-  filteredAssignments: Assignment[] = [];
-  filterText: string = '';
+
+
 
   constructor(private assignmentService: AssignmentService) { }
 
@@ -26,7 +27,6 @@ export class CarouselComponent implements OnInit {
     this.assignmentService.getAssignments().subscribe({
       next: (data: Assignment[]) => {
         this.assignments = data;
-        this.filteredAssignments = data;
       },
       error: (error) => {
         console.error('Erreur lors du chargement des assignments', error);
@@ -38,26 +38,12 @@ export class CarouselComponent implements OnInit {
     if(confirm("Voulez-vous vraiment supprimer cet assignment ?")){
       this.assignmentService.deleteAssignment(id).subscribe({
         next :()=>{
-          // Mise à jour de la liste après suppression
           this.assignments = this.assignments.filter(a => a.id !== id);
-          this.applyFilter();
         },
         error : (error) => {
           console.error('Erreur lors de la suppression', error);
         }
       });
-    }
-  }
-
-  applyFilter(): void {
-    if (this.filterText) {
-      this.filteredAssignments = this.assignments.filter(a =>
-        a.stock_industry.toLowerCase().includes(this.filterText.toLowerCase()) ||
-        a.stock_sector.toLowerCase().includes(this.filterText.toLowerCase()) ||
-        a.department.toLowerCase().includes(this.filterText.toLowerCase())
-      );
-    } else {
-      this.filteredAssignments = this.assignments;
     }
   }
 
