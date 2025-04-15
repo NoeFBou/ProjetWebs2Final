db0fd1b4005ba4346276b68e6bafa29ada69268b
NoeFBou <102726520+NoeFBou@users.noreply.github.com>
2025-04-15 18:44:30 +0200

    todo 2nd  projet

--- frontend-angular/src/app/filter/filter.component.ts
+++ frontend-angular/src/app/filter/filter.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import * as console from "node:console";
+import {Assignment, AssignmentService} from "../assignment.service";
 
 export interface FilterCriteria {
   name?: string;
@@ -10,17 +13,36 @@ export interface FilterCriteria {
   termine?: boolean;  // Filtrage sur le champ booléen
 }
 
+
+
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, AutoCompleteModule],
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+
   criteria: FilterCriteria = {};
 
+  names: string[] = [];
+  departments: string[] = [];
+  filteredNames: string[] = [];
+  filteredDepartments: string[] = [];
+
   @Output() filterChange = new EventEmitter<FilterCriteria>();
+
+  constructor(private assignmentService: AssignmentService) { }
+
+  ngOnInit(): void {
+    this.assignmentService.getAssignments().subscribe((assignments: Assignment[]) => {
+      this.names = assignments.map(a => a.name);
+      this.names = [...new Set(this.names)];
+      this.departments = assignments.map(a => a.department);
+      this.departments = [...new Set(this.departments)];
+    });
+  }
 
   onFilterChange(): void {
     console.log(this.criteria);
@@ -30,5 +52,15 @@ export class FilterComponent {
   resetFilters(): void {
     this.criteria = {};
     this.onFilterChange();
+  }
+
+  filterNames(event: { query: string }): void {
+    const query = event.query.toLowerCase();
+    this.filteredNames = this.names.filter(name => name.toLowerCase().includes(query));
+  }
+
+  filterDepartments(event: { query: string }): void {
+    const query = event.query.toLowerCase();
+    this.filteredDepartments = this.departments.filter(department => department.toLowerCase().includes(query));
   }
 }
